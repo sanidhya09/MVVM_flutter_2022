4f5d4396ef208138e56e4e6e9ccadb22c00b7272
sanidhya <sanidhya09@gmail.com>
2022-03-21 17:58:10 +0530

    added internet permissions

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,6 @@ void main() {
   runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
-      // This is the theme of your application.
-      //
-      // Try running your application with "flutter run". You'll see the
-      // application has a blue toolbar. Then, without quitting the app, try
-      // changing the primarySwatch below to Colors.green and then invoke
-      // "hot reload" (press "r" in the console where you ran "flutter run",
-      // or simply save your changes to "hot reload" in a Flutter IDE).
-      // Notice that the counter didn't reset back to zero; the application
-      // is not restarted.
       primarySwatch: Colors.red,
     ),
     home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -24,16 +15,6 @@ void main() {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -41,56 +22,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<String> entries = <String>['A', 'B', 'C'];
-  final List<int> colorCodes = <int>[600, 500, 100];
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: _buildBodyList(context),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-
-  // build list view & manage states
-  FutureBuilder<BaseModel<Post>> _buildBody(BuildContext context) {
-    return FutureBuilder<BaseModel<Post>>(
-      future: NetworkRepository().getResponseFromServer(2),
-      builder: (context, baseModel) {
-        if (baseModel.connectionState == ConnectionState.done) {
-          final Post? posts = baseModel.data?.data;
-          return _buildListView(context, posts);
-        } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-      },
     );
   }
 
@@ -101,7 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
       builder: (context, baseModel) {
         if (baseModel.connectionState == ConnectionState.done) {
           final List<Post>? posts = baseModel.data?.data;
-          return _buildListView2(context, posts);
+          return _buildListView(context, posts);
         } else {
           return const Center(
             child: CircularProgressIndicator(),
@@ -111,29 +49,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  // build list view & its tile
-  Widget _buildListView(BuildContext context, Post? posts) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            leading: const Icon(
-              Icons.account_box,
-              color: Colors.green,
-              size: 50,
-            ),
-            title: Text(
-              posts!.title,
-              style: const TextStyle(fontSize: 20),
-            ),
-            subtitle: Text(posts.body),
-          ),
-        );
-      },
-      itemCount: 1,
-    );
-  }
-  Widget _buildListView2(BuildContext context, List<Post>? posts) {
+  Widget _buildListView(BuildContext context, List<Post>? posts) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return Card(
@@ -155,23 +71,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-// ListView.separated(
-// padding: const EdgeInsets.all(8),
-// itemCount: entries.length,
-// itemBuilder: (BuildContext context, int index) {
-// return SizedBox(
-// height: 50,
-// child: Card(
-// semanticContainer: true,
-// color: Colors.amber[colorCodes[index]],
-// child: InkWell(
-// onTap: () {
-// print("tapped");
-// },
-// child: Center(child: Text('Entry ${entries[index]}')),
-// ),
-// ),
-// );
-// },
-// separatorBuilder: (BuildContext context, int index) => const Divider(),
-// )
